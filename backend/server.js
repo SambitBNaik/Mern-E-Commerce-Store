a02b827b6b05b3ad84e377f9db0772e6d087c83a
Sambit Basant Naik <sambitbasantnaik@gmail.com>
2024-11-15 09:45:06 +0530

    changes in frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
+
 
 
 import { connectDB } from './lib/db.js';
@@ -22,6 +25,8 @@ app.use("/api/auth",authRoutes);
 app.use("/api/products",productRoutes);
 app.use("/api/cart",cartRoutes);
 app.use("/api/coupon",couponRoutes);
+app.use("/api/payment",paymentRoutes);
+app.use("/api/analytics",analyticsRoutes);
 
 app.listen(PORT,()=>{
     console.log("Server is listing to on http://localhost:"+PORT);
